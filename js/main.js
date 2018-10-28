e0d5a5c7ee35f97dd567e52506e2622d3a1ee693
Danqin1 <piotr.dangquoc@gmail.com>
2018-10-28 10:40:27 +0100

    day2

--- js/main.js
+++ js/main.js
@@ -2,33 +2,31 @@ window.onload = function () {
     //console.log("loaded")
     const form = document.getElementById('form');
     form.addEventListener('submit', load);
-    const city = document.getElementById("city");
+    var city = document.getElementById("city");
     var d = new Date();
-    const tommorow = document.querySelector('#tommorow')
-    tommorow.innerHTML = d.getDate()+1+"."+(d.getMonth()+1)
+
     const secondDay = document.querySelector('#secondDay')
     secondDay.innerHTML = d.getDate()+2+"."+(d.getMonth()+1)
     const thirdDay = document.querySelector('#thirdDay')
     thirdDay.innerHTML = d.getDate()+3+"."+(d.getMonth()+1)
 
-
-    function load(e) {
+    function load (e) {
         e.preventDefault();
         let temp = document.getElementById("temp");
         let main = document.getElementById('main')
         let data
 
-        var xhttp = new XMLHttpRequest();
+        const xhttp = new XMLHttpRequest();
         xhttp.responseType = "json";
         xhttp.open('POST', 'http://api.openweathermap.org/data/2.5/weather?q=' + city.value +
             '&appid=0a6b59f9dfbf75051d17766f20204ab0&units=metric');
         xhttp.onreadystatechange = function () {
-            if (this.readyState = 4 && this.status == 200) {
+            if (this.readyState == 4 && this.status == 200) {
 
                 data = this.response
                 //console.log(typeof (this.response))
                 
-                temp.innerHTML = "Temperature: " + data.main.temp + " Celcius"
+                temp.innerHTML = "Temperature: " + data.main.temp + " °C"
                 main.innerHTML = "Weather: " + data.weather[0].main
                 if (data.weather[0].main == "Clouds") {
                     //console.log("cloud");
@@ -43,10 +41,6 @@ window.onload = function () {
                     document.body.style.background = "url('img/dawn.jpg')no-repeat"
                     document.body.style.backgroundSize = "cover"
                 }
-            } else {
-                name.innerHTML = "Wrong input value"
-                temp.innerHTML = ""
-                main.innerHTML = ""
             }
         }
         xhttp.send();
@@ -54,15 +48,33 @@ window.onload = function () {
     }
 
     function getForecast(){
-        var xhttp = new XMLHttpRequest();
+        let main1 = document.getElementById('main1');
+        let temp1 = document.getElementById('temp1');
+        let main2 = document.getElementById('main2');
+        let temp2 = document.getElementById('temp2');
+        let main3 = document.getElementById('main3');
+        let temp3 = document.getElementById('temp3');
+
+
+        const xhttp = new XMLHttpRequest();
         xhttp.responseType = "json";
-        xhttp.open('POST', 'http://api.openweathermap.org/data/2.5/forecast?q='+city.value+'&mode=xml&appid=0a6b59f9dfbf75051d17766f20204ab0');
+        xhttp.open('POST', 'http://api.openweathermap.org/data/2.5/forecast?q='+city.value+'&appid=0a6b59f9dfbf75051d17766f20204ab0&units=metric',true);
 
         xhttp.onreadystatechange = function () {
-            if (this.readyState = 4 && this.status == 200) {
+            if (this.readyState == 4 && this.status == 200) {
                 let data = this.response
-                console.log(data)
-                console.log('wys')
+                console.log(data.list[0].weather[0].main)
+                console.log(data.list[8].dt_txt)
+                console.log(data.list[16].dt_txt)
+                console.log(data.list[24].dt_txt)
+
+                main1.innerText = "Weather: " + data.list[8].weather[0].main;
+                main2.innerText = "Weather: " + data.list[16].weather[0].main;
+                main3.innerText = "Weather: " + data.list[24].weather[0].main;
+
+                temp1.innerText = "Temperature: " + Math.round(data.list[8].main.temp) + " °C";
+                temp2.innerText = "Temperature: " + Math.round(data.list[16].main.temp) + " °C";
+                temp3.innerText = "Temperature: " + Math.round(data.list[24].main.temp) + " °C";
             }
         
         }
